0244f343a752b3d2d3aa6c295d935a470969e454
sakurapleasure <sakurapleasure0@gmail.com>
2016-08-28 20:48:30 +0900

    changed main.d

--- main.d
+++ main.d
@@ -2,17 +2,39 @@
 
 import moeimg, std.net.curl, std.conv, std.string, std.stdio, std.file;
 
-void main(string[] args) {
+/*void main(string[] args) {
 	auto a = getArticles(parse!uint(args[1]));
-	auto b = getImages(a[0]);
 	if(!exists("imgs")) mkdir("imgs");
-	foreach(c; b) {
-		auto file = format("imgs/%s", c.filename);
-		if(!exists(file)) {
-			download(c.getURL(), file);
-			writeln("done: ", c.filename);
-		} else {
-			writeln("exists: ", c.filename);
+
+	foreach(article; a) {
+		auto dir = format("imgs/%s", article.name);
+		if(!exists(dir)) mkdir(dir);
+		auto images = getImages(article);
+
+		foreach(image; images) {
+			auto file = format("%s/%s", dir, image.filename);
+			if(!exists(file)) {
+				download(image.getURL(), file);
+				writeln("done: ", image.filename);
+			} else {
+				writeln("exists: ", image.filename);
+			}
 		}
 	}
+}*/
+
+void main(string[] args) {
+	int acount, icount;
+	auto num = parse!uint(args[1]);
+	foreach(i; 0..num) {
+		auto articles = getArticles(i+1);
+		acount += articles.length;
+		foreach(j, article; articles) {
+			writeln(article.name, '\t', j, '/', articles.length, "\t...");
+			icount += getImages(article).length;
+		}
+		writeln("done!");
+	}
+	writeln(acount, '\t', icount);
+	writeln(format("average: %s", cast(double)icount/cast(double)acount));
 }
